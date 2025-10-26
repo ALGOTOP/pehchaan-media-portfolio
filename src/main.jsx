15a0b3adab4a0d8eb1b251a09c7051c2ea00ca64
ALGOTOP <zainmalikdps@gmail.com>
2025-10-26 20:42:23 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";  // default import
+import {
+  AppProvider,
+  PortfolioErrorBoundaryV1,
+  BackgroundCanvasV1,
+  ThemeToggleV1,
+  ScrollToTopButtonV1,
+  HeroSection,
+  WorkSectionV1,
+  ServicesSectionV1,
+  AboutSectionV1,
+  ContactSectionV1,
+  FooterSectionV1,
+  useLenis,
+} from "./App";
 import "./index.css";
+
+function MainApp() {
+  useLenis();
+
+  return (
+    <AppProvider>
+      <PortfolioErrorBoundaryV1>
+        <BackgroundCanvasV1 />
+        <ThemeToggleV1 />
+        <ScrollToTopButtonV1 />
+        <main>
+          <HeroSection />
+          <WorkSectionV1 />
+          <ServicesSectionV1 />
+          <AboutSectionV1 />
+          <ContactSectionV1 />
+        </main>
+        <FooterSectionV1 />
+      </PortfolioErrorBoundaryV1>
+    </AppProvider>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <MainApp />
   </React.StrictMode>
 );
