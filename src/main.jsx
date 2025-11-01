4dc9cf1158d569f1233eba797d6f71b28aab4a35
ALGOTOP <zainmalikdps@gmail.com>
2025-11-02 04:31:32 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 import { useEffect } from "react";
 
 // ─────────────────────────────────────────────
