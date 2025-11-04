b3c4d7bf1033d7e89abd30e2e5d11903c50812a8
ALGOTOP <zainmalikdps@gmail.com>
2025-11-04 21:27:21 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "./", // <<< make assets relative so index.html works anywhere
+  base: "/", // <<< change to absolute path for Vercel
   plugins: [react()],
   resolve: {
     alias: {
