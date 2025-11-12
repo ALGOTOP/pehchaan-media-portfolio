b775734c792f5939937eb085550422196bfee683
mehmoodalam3650-cmd <saaenwear@gmail.com>
2025-11-12 06:55:04 +0500

    Update newsletter.js

--- api/newsletter.js
+++ api/newsletter.js
@@ -14,8 +14,8 @@ export default async function handler(req, res) {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: "infopehchaanmedia@gmail.com",
+      pass: soozywkmiwgpbbkl,
     },
   });
 
