add00dd5f3cbe9de3527d611a87259f1ec8ae18e
mehmoodalam3650-cmd <saaenwear@gmail.com>
2025-11-15 19:43:34 +0500

    Create WorkGrid.jsx

---
+++ src/components/work/WorkGrid.jsx
@@ -0,0 +1,24 @@
+// src/components/work/WorkGrid.jsx
+import React from "react";
+import WorkItem from "./WorkItem";
+
+export default function WorkGrid({ items, onOpen }) {
+  return (
+    <section className="pt-4 pb-20">
+      <div className="max-w-7xl mx-auto px-6">
+        <div
+          className="
+            grid grid-cols-1 
+            sm:grid-cols-2 
+            lg:grid-cols-3 
+            gap-8
+          "
+        >
+          {items.map((item, i) => (
+            <WorkItem key={item.id} item={item} index={i} onOpen={onOpen} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
